fb3c14614f679db3ff9ab5bdef54576494d1406d
Sherif Mostafa <sherifmostafa92@googlemail.com>
2019-03-23 09:23:33 -0700

    added welcome msg

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
 const app = express()
-app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.send('HEY!')
+})
 app.listen(3000, () => console.log('Server running on port 3000'))
