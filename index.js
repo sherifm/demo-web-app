0039d1d117850e0b6ade3969b0b0e0d0a06cfd71
Sherif Mostafa <sherifmostafa92@googlemail.com>
2019-03-23 09:22:03 -0700

    Fixed express typo

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-t express = require('express')
+const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY!')
-})
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
