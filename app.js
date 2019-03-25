97b1bfa6151455aae7f9ee918f4d0272762e3bcf
Sherif Mostafa <sherifmostafa92@googlemail.com>
2019-03-24 23:51:46 -0700

    dockerized mongo and yelpcamp app

--- app.js
+++ app.js
@@ -16,7 +16,37 @@ var commentRoutes 		= require("./routes/comments"),
 	indexRoutes 		= require("./routes/index")
 
 const port = 3000; 
- mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true }); 
+
+const options = {
+    autoIndex: false, // Don't build indexes
+    reconnectTries: 30, // Retry up to 30 times
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
+  }
+
+
+ const connectWithRetry = () => {
+  console.log('MongoDB connection with retry')
+  mongoose.connect("mongodb://mongo:27017/yelp_camp", options).then(()=>{
+    console.log('MongoDB is connected')
+  }).catch(err=>{
+    console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
+    console.log(err)
+    setTimeout(connectWithRetry, 5000)
+  })
+}
+
+connectWithRetry()
+
+//Docker service connection
+// mongoose.connect('mongodb://<docker_service>:27017/yelp_camp', { useNewUrlParser: true });
+
+//Local-host connection 
+// mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
+
+//Heroku-connection
 //mongoose.connect('mongodb+srv://sherifm:XrayNT12010!@cluster0-uzzxj.mongodb.net/yelp_camp?retryWrites=true'); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
